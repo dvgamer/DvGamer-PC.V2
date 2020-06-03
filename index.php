0724d0345682df6f5f7d3e202e6b2fe52e49e4a7
Kananek Thongkam <info.dvgamer@gmail.com>
2020-06-04 00:21:06 +0700

    fixed php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php 
-if(ereg("(/?@)",$_SERVER['REQUEST_URI'])): ///?@m!mod_online.php
+if(preg_match("/(\?@)/",$_SERVER['REQUEST_URI'])): ///?@m!mod_online.php
   ob_start(); 
   foreach (glob("_require/*.php") as $_file_name) include_once($_file_name);
   $database = new SyncDatabase();
@@ -16,16 +16,15 @@ elseif(isset($_REQUEST['html'])):
 	foreach (glob("site/css/*.css") as $_file_name) echo '<link rel="stylesheet" type="text/css" href="'.$_file_name.'" />'."\n\r"; 
 	foreach (glob("plugins/*.js") as $_file_name) echo '<script type="text/javascript" src="'.$_file_name.'"></script>'."\n\r"; 
 	if(file_exists('module/iframe/'.$_REQUEST['html'].'.html')) include_once('module/iframe/'.$_REQUEST['html'].'.html'); else include_once('plugins/default.php');
-elseif(ereg("(/?)|(/)", $_SERVER['REQUEST_URI']) || $_SERVER['REQUEST_URI']=='/'):
+elseif(preg_match("/\?|\//", $_SERVER['REQUEST_URI']) || $_SERVER['REQUEST_URI']=='/'):
 // HTML index page.
+date_default_timezone_set("Asia/Bangkok");
+
 ob_start(); 
 ?>
 <!doctype html>
 <head>
 <?php
-error_reporting(-1);
-date_default_timezone_set("Asia/Bangkok");
-
 // Configuration \\
 foreach(glob("_require/*.php") as $_file_name) include_once($_file_name);
 include_once("include/language.php");
